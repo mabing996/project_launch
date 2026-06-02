3dc203d61473add1f1e658d2c8c486b138df8182
Your Name <you@example.com>
2026-06-02 17:39:31 +0800

    1

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 
 
 
-with open("/Users/swh/project_launch/project_data.json", "r", encoding="utf-8") as f:
+with open("./project_data.json", "r", encoding="utf-8") as f:
     project_data_dict = json.load(f)
 res = pipeline(project_data_dict=project_data_dict)
 print(res)
